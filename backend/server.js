5bc3dfec30efbab3d39928ee18cf6a9669b8d4ee
Ali Sina <ali.sina1447@gmail.com>
2024-08-24 14:39:18 -0400

    initialize React app for frontend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Sinaali123.",
+    database: "todoapp"
+})
+
+app.get('/', (req, res) => {
+    const sqlQuery = "SELECT * FROM TASKS";
+
+    db.query(sqlQuery, (err, data) => {
+        if (err) {
+            res.json("Error with database");
+        }
+
+        res.json(data);
+
+    })
+})
+
 
 app.listen(8086, () => {
     console.log("Hello from backend!");
